0f20a7a9f2a800e29efd8f8c98da498c5f3b7198
Wycliffe cheruiyot <53683890+cliffkibz@users.noreply.github.com>
2025-03-25 15:08:52 +0000

    comments

--- motion.ino
+++ motion.ino
@@ -4,17 +4,14 @@
 void setup(){
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(SENSOR_PIN, INPUT);
-  
 }
-
 void loop(){
   int sensorValue = digitalRead(SENSOR_PIN);
-  if (sensorValue == HIGH)
-  {
-    digitalWrite(RELAY_PIN, LOW);
-  }
-  else
-  {
+  if (sensorValue == HIGH){//If movement is detected the bulb turns on
+    digitalWrite(RELAY_PIN, LOW); //stays on for 3 seconds
+  } 
+
+  else { //else it stays off
     digitalWrite(RELAY_PIN, HIGH);
   }
 }
